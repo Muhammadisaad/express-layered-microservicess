3236e036a63b5241f72f12bb75eaf46923a7e4ba
muhammadisaadi <muhammadisaad0981@gmail.com>
2026-08-16 14:46:26 +0500

    feat:initial commit for express setup

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+  res.send('Hello, World! nodemon configured');
+});
+
+app.listen(PORT,()=>{
+
+    console.log(`server is running on port ${PORT}`)
+})
