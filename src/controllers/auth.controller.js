607482a54c195306a0fb0796a40444a3bbdb3385
muhammadisaadi <muhammadisaad0981@gmail.com>
2026-08-17 18:04:47 +0500

    refactor(auth):swithc registration to DB-level unique constraint and 1100 error handling Removed manual User.findone() pre-check to avoid race condition catch mongoDB duplicate key error (code 11000) to return 409 conflict

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,46 +4,40 @@ import User from "../models/register.model.js"
     try {
         const {name , email ,password,role}= req.body;
 
-        //check if user already exist
-         const existingUser = await User.findOne({email});
-         if(existingUser){
-            return res.status(400).json(
-                {
-                    success:false,
-                    message:"user aready exist"
-                }
-            )
-         }
-         //create user if not exists
-         const user = await User.create({
-            name,
-            email,
-            password,
-            role
-         })
-         //give response data
-         const userResponse ={
+
+        const user = await User.create({
+            name,email,password,role:'user'
+        });
+        const userResponse ={
             name:user.name,
             email:user.email,
             role:user.role,
             createdat:user.createdAt
          };
-         res.status(201).json({
+        return res.status(201).json({
             success:true,
             message:"user registered successfully",
-            data:userResponse   
-         })
-
-
-
-    } catch (error) {
-        console.error("REGISTER CONTROLLER ERROR:", error);
-        res.status(500).json({
-            success:false,
-            message:"internal server error"
+             data:userResponse   
+            
         });
         
-    };
+
+    }catch(error)
+    {
+            if(error.code=11000){
+                return res.status(409).json({
+                    success:false,
+                    message:"user already exist",
+                    error:error.message
+                });
+            }
+            return res.status(500).json({
+                success:false,
+                message:"internal server error"
+            });
+        }
+
+        
 }
 export default registerUser;
 
